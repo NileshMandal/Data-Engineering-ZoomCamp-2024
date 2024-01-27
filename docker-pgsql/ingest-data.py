5bbf2afff57001b6796839793c174e970c5cf559
Nileshkumar Mandal <68818014+NileshMandal@users.noreply.github.com>
2024-01-27 16:23:52 +0000

    update

--- docker-pgsql/ingest-data.py
+++ docker-pgsql/ingest-data.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 import argparse
 import os
 import pandas as pd
@@ -19,42 +15,26 @@ def main(params):
 
     csv_name = 'output.csv'
 
-    os.system(f"wget {url} -O {csv_name}")
+    if url.endswith('.gz'):
+        os.system(f"wget {url} -O - | gzip -d > {csv_name}")
+    else:
+        os.system(f"wget {url} -O {csv_name}")
 
-    engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{db}'.format(
-    user=user,
-    password=password,
-    host=host,
-    port=port,
-    db=db))
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iterator = pd.read_csv(csv_name, compression='gzip',low_memory=False, iterator=True, chunksize=100000)
+    df_iterator = pd.read_csv(csv_name, compression='gzip' if url.endswith('.gz') else None, low_memory=False, iterator=True, chunksize=100000)
 
     df = next(df_iterator)
 
-#    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.head(0).to_sql(name=table, con=engine, if_exists='replace', index=False)
 
-#    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
-
-
-    df.head(0).to_sql(name=table,con = engine,if_exists = 'replace')
-
-    df.to_sql(name=table,con = engine,if_exists = 'append')
+    df.to_sql(name=table, con=engine, if_exists='append', index=False)
 
     while True:
         try:
             t_start = time()
             df = next(df_iterator)
-
- #           df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
-
-#            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
-
-            df.to_sql(name=table, con=engine, if_exists='append')
+            df.to_sql(name=table, con=engine, if_exists='append', index=False)
             t_end = time()
 
             print('Inserted another chunk..., time took:', (t_end - t_start))
@@ -64,18 +44,14 @@ def main(params):
             break
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Ingest csv file to Postgres Table.')
-
-    parser.add_argument('--user', help='username for postgres')
-    parser.add_argument('--password', help='password for postgres')
-    parser.add_argument('--host', help='host for postgres')
-    parser.add_argument('--port', help='port for postgres')
-    parser.add_argument('--db', help='database name for postgres')
-    parser.add_argument('--table', help='table name of postgres')
+    parser = argparse.ArgumentParser(description='Ingest csv file to PostgreSQL Table.')
+    parser.add_argument('--user', help='username for PostgreSQL')
+    parser.add_argument('--password', help='password for PostgreSQL')
+    parser.add_argument('--host', help='host for PostgreSQL')
+    parser.add_argument('--port', help='port for PostgreSQL')
+    parser.add_argument('--db', help='database name for PostgreSQL')
+    parser.add_argument('--table', help='table name of PostgreSQL')
     parser.add_argument('--url', help='url for the csv file')
 
     args = parser.parse_args()
-
     main(args)
-
